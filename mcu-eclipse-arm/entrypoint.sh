d2c047e66f6a78d1d668a71f37c74ab4e602499b
Paul Melnikov <positron96@gmail.com>
2019-12-23 14:30:07 +0700

    added separate script for build

--- mcu-eclipse-arm/entrypoint.sh
+++ mcu-eclipse-arm/entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 PROJ=$1
 TARGET=$2
-eclipse --launcher.suppressErrors -nosplash -application org.eclipse.cdt.managedbuilder.core.headlessbuild -data /workspace -import /workspace/$PROJ -cleanBuild $PROJ/$TARGET
+eclipse-build /workspace $PROJ $TARGET
