1854e9579639ec8fff4dddd6ab2d302ffa08bd0e
Paul Melnikov <positron96@gmail.com>
2020-08-13 16:31:29 +0700

    Replaced with one binary and case. Added -f -c -d options

--- git-version-saver/ver_incl.sh
+++
@@ -1,8 +0,0 @@
-REPO=${2:-"."}
-COMMIT=${3:-HEAD}
-
-DATE=$(git -C $REPO show --format="%cd" --date=short -s $COMMIT)
-DATE_SHORT=$(git -C $REPO show --format="%cd" --date=format:%Y%m%d -s $COMMIT)
-HASH=$(git -C $REPO show --format="%h" -s $COMMIT)
-TAG=$(git -C $REPO describe --tags $COMMIT | sed 's/-g[a-z0-9]\{7\}//')
-VER_SHORT="${DATE_SHORT}-$(echo $TAG | tr '\\' '_')-$HASH"
